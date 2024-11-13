fdfc2863f7fa493902eb6db0d9bdb12ead9d9394
CodeWithClinton <clintonmatics@gmail.com>
2024-11-13 01:26:20 +0100

    intro and project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+
+
+const App = () => {
+  return (
+    <div>
+      Hello World
+      <Button className="bg-blue-800">Click me</Button>
+    </div>
+  )
+}
+
+export default App
