d3ed6eb46e76b92d811b7d2c1b9ecfe1e0b1b329
CodeWithClinton <clintonmatics@gmail.com>
2024-11-16 01:39:51 +0100

    fetching blogs with react query

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,26 @@
+import { getBlogs } from "@/services/apiBlog";
 import BlogContainer from "@/ui_components/BlogContainer";
 import Header from "@/ui_components/Header";
+import { useQuery } from "@tanstack/react-query";
 
 const HomePage = () => {
+
+
+  const {isPending, isError, error, data:blogs} = useQuery({
+    queryKey: ['blogs'],
+    queryFn: getBlogs
+  })
+
+  console.log(blogs)
+
+
+
+
+
   return (
     <>
       <Header />
-      <BlogContainer />
+      <BlogContainer isPending={isPending}  blogs={blogs} />
     </>
   );
 };
